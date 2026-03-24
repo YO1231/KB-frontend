e93e6d6460eda8320ae3fa3a5e68e9ee9fb49bb2
YO1231 <whee0211@gmail.com>
2026-03-24 17:48:31 +0900

    feat: 15일차 실습 및 과제

---
+++ AM/03_VUE/ch07/test-vue-app2/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import CheckboxItem from './components/CheckboxItem.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+</script>
+
+<template>
+  <div>
+    <h2>App 컴포넌트</h2>
+    <hr />
+    <ul>
+      <CheckboxItem />
+      <!-- checkbox-item -->
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+};
+</script>
+
+<style scoped></style>
