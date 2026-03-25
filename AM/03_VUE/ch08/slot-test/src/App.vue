e052677bbc4a61bec5051322988f80e2f9f87f3d
YO1231 <whee0211@gmail.com>
2026-03-25 13:29:53 +0900

    feat: 16일차 오전 실습

---
+++ AM/03_VUE/ch08/slot-test/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NoSlotTest />
+    <NamedSlotTest />
+  </div>
+</template>
+
+<script>
+import NoSlotTest from './components/NoSlotTest.vue';
+import NamedSlotTest from './components/NamedSlotTest.vue';
+
+export default {
+  name: 'App',
+  components: { NoSlotTest, NamedSlotTest },
+};
+</script>
