ff0c6c4a46e22f42e587a2bf22a1a05b020d1bf9
YO1231 <whee0211@gmail.com>
2026-03-27 17:47:34 +0900

    feat: 18일차 과제

---
+++ PM/adv/day18/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /> <br />
+    결과 : {{ result }}
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  name: 'Calc5',
+  setup() {
+    const x = ref(10);
+    const result = ref(0);
+
+    watch(x, (c, o) => {
+      console.log(`${o} -> ${c}`);
+      result.value = c * 2;
+    });
+
+    return { x, result };
+  },
+};
+</script>
