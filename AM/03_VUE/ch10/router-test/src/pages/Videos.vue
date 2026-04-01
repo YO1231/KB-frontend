47c6f0d76d4719fb9770bed204b1329537b59fd6
YO1231 <whee0211@gmail.com>
2026-04-01 13:14:21 +0900

    feat: 21일차 오전 실습

---
+++ AM/03_VUE/ch10/router-test/src/pages/Videos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card card-body">
+    <h2 class="m-3">영상 리스트</h2>
+    <ul class="list-group">
+      <li
+        v-for="v in videos"
+        :key="v.id"
+        class="list-group-item text-left"
+        :class="playingVideo(v.id)"
+      >
+        {{ v.title }} ({{ v.category }})
+        <router-link :to="{ name: 'videos/id', params: { id: v.id } }">
+          <span class="float-end badge bg-secondary">듣기</span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { YoutubeVue3 } from 'youtube-vue3';
+
+export default {
+  name: 'VideoPlayer',
+  components: { YoutubeVue3 },
+  setup() {
+    const stopVideo = () => {
+      playerRef.value.player.stopVideo();
+      router.push({ name: 'videos' });
+    };
+  },
+};
+</script>
