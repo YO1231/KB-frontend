6074e64e7c8e05b6b6df0fd438d363baa27d20ef
YO1231 <whee0211@gmail.com>
2026-04-02 17:48:39 +0900

    feat: 22일차 오후 실습 및 과제1

---
+++ PM/adv/day22/axios-test-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>콘솔을 확인합니다.</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const listUrl = '/api/todos';
+const todoUrlPrefix = '/api/todos/';
+
+const requestAPI = () => {
+  let todoList = [];
+
+  axios
+    .get(listUrl)
+    .then((response) => {
+      todoList = response.data;
+      console.log('# TodoList : ', todoList);
+      return todoList[0].id;
+    })
+    .then((id) => {
+      return axios.get(todoUrlPrefix + id);
+    })
+    .then((response) => {
+      console.log('## 첫번째 Todo : ', response.data);
+    });
+};
+
+requestAPI();
+</script>
